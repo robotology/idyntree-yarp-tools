9c480f72fb87c6eec9e2c09711101d3770b226a0
Silvio Traversaro <silvio.traversaro@iit.it>
2022-11-17 16:15:49 +0100

    Fix compatibility with YARP 3.8

--- src/lib/ReadOnlyRemoteControlBoard/stateExtendedReader.h
+++ src/lib/ReadOnlyRemoteControlBoard/stateExtendedReader.h
@@ -28,7 +28,6 @@
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/ControlBoardInterfacesImpl.h>
 #include <yarp/dev/ControlBoardHelpers.h>
-#include <yarp/dev/PreciselyTimed.h>
 
 #include <yarp/dev/impl/jointData.h>
 
